0b1a463f47dc6c1b693fc70eb510aa0cc39c0a99
Катасонова Мария <katasonova_masa@mail.ru>
2023-03-01 21:03:05 +0300

    add new method

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello qa.guru");
+        System.out.println(getMessage());
     }
+    private static String getMessage() {return "No";}
 }
