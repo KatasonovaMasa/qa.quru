55f0b03038ad9e86b3d901f18b1b6271ff68077c
Катасонова Мария <44576709+KatasonovaMasa@users.noreply.github.com>
2023-03-02 22:17:41 +0300

    Delete Main.java

--- src/test/java/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    private static String msg = "Hi";
-
-    public static void main(String[] args) {
-        System.out.println(getMessage());
-    }
-    private static String getMessage() {return "No";}
-}
-
