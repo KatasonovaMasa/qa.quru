3d73472a3411ab4d049d10dd69f2953108a1c5d8
Катасонова Мария <katasonova_masa@mail.ru>
2023-03-04 17:09:44 +0300

    zadaniye 4

--- src/test/java/WritePracticeFormTest.java
+++ src/test/java/WritePracticeFormTest.java
@@ -2,15 +2,11 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
-
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
-import static org.openqa.selenium.Keys.ENTER;
 
-public class WritePracticeForm {
+public class WritePracticeFormTest {
     @BeforeAll
     static void beforeAll(){
         Configuration.browserSize = "1920x1080";
