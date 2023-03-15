a4b39fae5a611d062fc931c5fe543293bf5770e8
Катасонова Мария <katasonova_masa@mail.ru>
2023-03-15 23:10:44 +0300

    открытие 2 страниц на гитхабе 3-мя способами

--- src/test/java/tests/othersDZ/OpenGitHubPagesTest.java
+++ src/test/java/tests/othersDZ/OpenGitHubPagesTest.java
@@ -3,23 +3,24 @@ package tests.othersDZ;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Tags;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.stream.Stream;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
+
 
 public class OpenGitHubPagesTest {
     @BeforeAll
     static void beforeAll(){
         Configuration.browserSize = "1920x1080";
     }
-
     @BeforeEach
     void setup() {
         Selenide.open("https://github.com/");
@@ -35,17 +36,47 @@ public class OpenGitHubPagesTest {
             @Tag("BLOCKER"),
             @Tag("WEB")
     })
-
     void OpenGitHubPagesTest(String testData, String expectedText){
-        //Откойте страницу Selenide в Github
-        //    open("https://github.com/");
         $("[aria-label=\"Search GitHub\"]").setValue("selenide").pressEnter();
         $$("ul.repo-list li").first().$("a").click();
         $("#repository-container-header").shouldHave(text("selenide / selenide"));
-        //Перейдите в раздел Wiki проекта
         $("#wiki-tab").click();
-        //Откройте страницу {0}, проверьте что внутри есть пример кода для JUnit5
-        $("[id='wiki-pages-filter']").setValue(testData).click();
-        $("[id=\"wiki-content\"]").shouldHave(Condition.text(expectedText)).click();
+        $("#wiki-pages-filter").setValue(testData).click();
+        $("#wiki-content").shouldHave(Condition.text(expectedText)).click();
+    }
+    @ValueSource(strings ={
+            "SoftAssertions", "Snippets"
+        }
+    )
+    @ParameterizedTest
+    @DisplayName("Открытие на гитхабе страниц {0} должен отображаться текст {0}")
+    @Tags({
+            @Tag("BLOCKER"),
+            @Tag("WEB")
+    })
+    void OpenGitHubPagesTest2(String testData){
+        $("[aria-label=\"Search GitHub\"]").setValue("selenide").pressEnter();
+        $$("ul.repo-list li").first().$("a").click();
+        $("#repository-container-header").shouldHave(text("selenide / selenide"));
+        $("#wiki-tab").click();
+        $("#wiki-pages-filter").setValue(testData).click();
+        $("#wiki-content").shouldHave(text(testData));
+    }
+    static Stream<String> argumentsStream() {
+        return Stream.of("SoftAssertions", "Snippets");
+    }
+    @MethodSource("argumentsStream")
+    @ParameterizedTest(name = " {0}")
+    @Tags({
+            @Tag("BLOCKER"),
+            @Tag("WEB")
+    })
+    void OpenGitHubPagesTest3(String testData) {
+        $("[aria-label=\"Search GitHub\"]").setValue("selenide").pressEnter();
+        $$("ul.repo-list li").first().$("a").click();
+        $("#repository-container-header").shouldHave(text("selenide / selenide"));
+        $("#wiki-tab").click();
+        $("#wiki-pages-filter").setValue(testData).click();
+        $("#wiki-content").shouldHave(text(testData));
     }
 }
