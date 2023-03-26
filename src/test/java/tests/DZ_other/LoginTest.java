86b997efcbfa2bf9ac5c645bf856a1eaea821152
Катасонова Мария <katasonova_masa@mail.ru>
2023-03-26 17:37:11 +0300

    add all qa_quru_dz

--- src/test/java/tests/DZ_other/LoginTest.java
+++ src/test/java/tests/DZ_other/LoginTest.java
@@ -1,11 +1,14 @@
 package tests.DZ_other;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
+
+@Disabled
 
 public class LoginTest {
     @Test
