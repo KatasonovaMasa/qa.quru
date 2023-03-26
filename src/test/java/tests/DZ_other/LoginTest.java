abfd4edd5dbb4d40da2f80e1794dae5f4cf0b783
Катасонова Мария <katasonova_masa@mail.ru>
2023-03-26 18:02:06 +0300

    add all qa_quru_dz

--- src/test/java/tests/DZ_other/LoginTest.java
+++ src/test/java/tests/DZ_other/LoginTest.java
@@ -1,34 +1,50 @@
 package tests.DZ_other;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.testng.annotations.AfterTest;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 
-@Disabled
-
 public class LoginTest {
+    @AfterTest
     @Test
     void successfulLoginTest(){
         open("https://qa.guru/cms/system/login");
-        $("[name=email]").setValue("Mariya.Katasonova@nedra.digital");
-        $("[name=password]").setValue("b&D32ap+3Q");
+        $("[name=email]").setValue("qagurubot@gmail.com");
+        $("[name=password]").setValue("qagurupassword");
         $(".btn-success").click();
         $(".main-header__login").click();
-        $(".logined-form").shouldHave(text("Мария"));
+        $(".logined-form").shouldHave(text("QA_GURU_BOT"));
     }
+    @AfterTest
     @Test
     void successfulLoginWithCommentsTest(){
+        //        Открыть форму авторизации
+//        https://qa.guru/cms/system/login
+//        Ввести адрес электронной почты
+//        Ввести пароль
+//        Нажать кнопку "Войти"
+//        Нажать на кнопку "Личный кабинет"
+//        Проверить успешную авторизацию
 
+//        Открыть форму авторизации
         open("https://qa.guru/cms/system/login");
-        $("[name=email]").setValue("Mariya.Katasonova@nedra.digital");
-        $("[name=password]").setValue("b&D32ap+3Q");
+//        Ввести адрес электронной почты
+        $("[name=email]").setValue("qagurubot@gmail.com");
+//        Ввести пароль
+        $("[name=password]").setValue("qagurupassword");
+//        Нажать кнопку "Войти"
         $(".btn-success").click();
+//        Нажать на кнопку "Личный кабинет"
         $(".main-header__login").click();
-        $(".logined-form").shouldHave(text("Мария"));
+//        Проверить успешную авторизацию
+        $(".logined-form").shouldHave(text("QA_GURU_BOT"));
+}
+    @AfterTest
+    public void exitTest(){
+        $("button[text='Сменить пользователя']");
     }
-
 }
