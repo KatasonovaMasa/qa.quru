77444f18311a2b0d57fda931aaa7b1b563df6a5a
Катасонова Мария <katasonova_masa@mail.ru>
2023-05-03 23:01:30 +0300

    апи тесты

--- src/test/java/tests/dz_21/CreateTestcaseTests.java
+++ src/test/java/tests/dz_21/CreateTestcaseTests.java
@@ -19,17 +19,11 @@ import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 import static io.qameta.allure.Allure.step;
 import static io.restassured.RestAssured.given;
 import static java.lang.String.format;
-import static org.assertj.core.api.Assertions.assertThat;
 
 
 @Tag("api_allure")
 public class CreateTestcaseTests extends TestBase {
 
-    @BeforeAll
-    static void beforeAll(){
-        Configuration.browserSize = "1920x1080";
-        Configuration.baseUrl = "https://allure.autotests.cloud";
-    }
     RegistrationPage registrationPage = new RegistrationPage();
     static String login = "allure8",
             password = "allure8",
@@ -44,36 +38,6 @@ public class CreateTestcaseTests extends TestBase {
     }
 
 
-    @Test
-    void createWitApiOnlyTest() {
-        Faker faker = new Faker();
-        String testCaseName = faker.name().fullName();
-
-        CreateTestCaseBody testCaseBody = new CreateTestCaseBody();
-        testCaseBody.setName(testCaseName);
-
-        CreateTestCaseResponse createTestCaseResponse = step("Create testcase", () ->
-                given()
-                        .log().all()
-                        .header("X-XSRF-TOKEN", "91590414-274a-4cfa-bb89-9ef757a606aa")
-                        .cookies("XSRF-TOKEN", "91590414-274a-4cfa-bb89-9ef757a606aa",
-                                "ALLURE_TESTOPS_SESSION", "dce276c1-dbed-44fd-ac4c-0eb41166e73b")
-                        .contentType("application/json;charset=UTF-8")
-                        .body(testCaseBody)
-                        .queryParam("projectId", projectId)
-                        .when()
-                        .post("/api/rs/testcasetree/leaf")
-                        .then()
-                        .log().status()
-                        .log().body()
-                        .statusCode(200)
-                        .extract().as(CreateTestCaseResponse.class)
-        );
-
-        step("Verify testcase name", () ->
-                assertThat(createTestCaseResponse.getName()).isEqualTo(testCaseName));
-
-    }
     @Test
     void createWitApiOnlyUiTest() {
         Faker faker = new Faker();
